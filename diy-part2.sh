855aebb38922ea21a19d3541023693b55cb43437
魏婴iii <83204053+XiaBai0601@users.noreply.github.com>
2023-11-29 14:40:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,14 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+# 主题
+git clone https://github.com/DHDAXCW/theme
+svn export https://github.com/jerrykuku/luci-theme-argon/branches/18.06 luci-theme-argon
+svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-argon-config
+rm -rf ../../customfeeds/luci/themes/luci-theme-argon
+#rm -rf ./luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp -f $GITHUB_WORKSPACE/images/bg1.jpg luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg # 修改默认背景
+
 # 修复移远PCIe驱动(quectel_MHI)
 rm -rf package/wwan/driver/quectel_MHI
 svn export https://github.com/Siriling/5G-Modem-Support/trunk/quectel_MHI package/wwan/driver/quectel_MHI

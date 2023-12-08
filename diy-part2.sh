d8f61c072f301957eee68ad522281a19d3eca327
魏婴iii <83204053+XiaBai0601@users.noreply.github.com>
2023-12-09 07:22:25 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,13 +16,9 @@
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 cat >"$CLEAR_PATH" <<-EOF
-packages
 config.buildinfo
 feeds.buildinfo
 sha256sums
 version.buildinfo
-profiles.json
-openwrt-x86-64-generic-kernel.bin
-openwrt-x86-64-generic.manifest
-openwrt-x86-64-generic-squashfs-rootfs.img.gz
+openwrt-ramips-mt7621-xiaomi_mi-router-3g.manifest
 EOF

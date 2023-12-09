6563c533982f7d2300af6cc6325767905907bd37
魏婴iii <83204053+XiaBai0601@users.noreply.github.com>
2023-12-09 13:12:31 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# 修改主机名称为OpenWrt-123(填0为不作修改)
+export Op_name="Mi-R3G"                
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate

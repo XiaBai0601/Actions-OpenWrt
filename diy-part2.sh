a572561745917db7c08150d0d8a82efb9f01b122
魏婴iii <83204053+XiaBai0601@users.noreply.github.com>
2023-12-10 05:58:35 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,11 +16,4 @@ export Op_name="MI-R3G"
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
-cat >"$CLEAR_PATH" <<-EOF
-config.buildinfo
-feeds.buildinfo
-sha256sums
-version.buildinfo
-openwrt-ramips-mt7621-xiaomi_mi-router-3g.manifest
-EOF
+

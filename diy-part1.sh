4eb6b218b2d48e4336273d5873117ebcf0a04111
魏婴iii <83204053+XiaBai0601@users.noreply.github.com>
2023-12-10 06:08:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,12 +15,3 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-# 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
-cat >"$CLEAR_PATH" <<-EOF
-config.buildinfo
-feeds.buildinfo
-sha256sums
-version.buildinfo
-openwrt-ramips-mt7621-xiaomi_mi-router-3g.manifest
-EOF
